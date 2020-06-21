8b37ad1fd7964bdda790d86587fcd4fda77ef38c
DGromek <dawidgromek.1997@gmail.com>
2020-06-21 13:41:59 +0200

    Adding vm to server.

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,13 +1,28 @@
 package edu.iis.mto.serverloadbalancer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
+
     private int capacity;
+    private int filledCapacity = 0;
+    private List<Vm> vms = new ArrayList<>();
 
     public Server(int capacity) {
         this.capacity = capacity;
     }
 
-    public int getCapacity() {
-        return capacity;
+    public double getFillPercentage() {
+        return filledCapacity * 1.0d / capacity * 100.0;
+    }
+
+    public void addVm(Vm vm){
+        vms.add(vm);
+        filledCapacity += vm.getSize();
+    }
+
+    public boolean contains(Vm vm) {
+        return this.vms.contains(vm);
     }
 }
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -3,18 +3,30 @@ package edu.iis.mto.serverloadbalancer;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ServerLoadBalancerTest {
 
-    @Test
-    public void oneServerAndNoVmsShouldResultInServerWithNoVms() {
-        Server[] servers = {new Server(10)};
-        Vm[] vms = new Vm[0];
-        ServerLoadBalancer serverLoadBalancer = new ServerLoadBalancer();
+	@Test
+	public void oneServerAndNoVmsShouldResultInServerWithNoVms() {
+		Server[] servers = {new Server(10)};
+		Vm[] vms = new Vm[0];
+		ServerLoadBalancer serverLoadBalancer = new ServerLoadBalancer();
 
-        serverLoadBalancer.balance(servers, vms);
+		serverLoadBalancer.balance(servers, vms);
 
-        assertEquals(0, servers[0].getFillPercentage(), 0.001);
-    }
+		assertEquals(0, servers[0].getFillPercentage(), 0.001);
+	}
 
+	@Test
+	public void oneServerWith10CapacityAndOneVmWith10SizeShouldResultInFullyFilledServer() {
+		Server[] servers = {new Server(10)};
+		Vm[] vms = {new Vm(10)};
+		ServerLoadBalancer serverLoadBalancer = new ServerLoadBalancer();
+
+		serverLoadBalancer.balance(servers, vms);
+		assertEquals(100.0d, servers[0].getFillPercentage(), 0.001);
+		assertTrue(servers[0].contains(vms[0]));
+	}
+	
 }
