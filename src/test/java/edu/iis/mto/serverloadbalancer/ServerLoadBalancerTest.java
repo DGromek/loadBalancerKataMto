c0fec46b0b17b9e8ed0b647826c3a0c266bf05e4
DGromek <dawidgromek.1997@gmail.com>
2020-06-22 22:56:49 +0200

    Second test RED

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -3,6 +3,7 @@ package edu.iis.mto.serverloadbalancer;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ServerLoadBalancerTest {
 
@@ -17,4 +18,14 @@ public class ServerLoadBalancerTest {
         assertEquals(0, singleServerArray[0].getFillPercentage(), 0.001);
     }
 
+    @Test
+    public void oneServerWith10CapacityAndOneVmWith10SizeShouldResultInFullyFilledServer() {
+        ServerLoadBalancer serverLoadBalancer = new ServerLoadBalancer();
+        Server[] singleServerArray = {new Server(10)};
+        Vm[] vms = {new Vm(10)};
+        serverLoadBalancer.balance(singleServerArray, vms);
+
+        assertEquals(100.0d, singleServerArray[0].getFillPercentage(), 0.001);
+        assertTrue(singleServerArray[0].contains(vms[0]));
+    }
 }
