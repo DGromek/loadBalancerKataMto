d3645fb5d423dfb2a2e4c9d2e3f502c1a37c642f
DGromek <dawidgromek.1997@gmail.com>
2020-06-22 22:49:47 +0200

    First test RED

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -7,10 +7,16 @@ import static org.hamcrest.Matchers.equalTo;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
-	@Test
-	public void itCompiles() {
-		assertThat(true, equalTo(true));
-	}
 
+	@Test
+	public void oneServerAndNoVmsShouldResultInServerWithNoVms() {
+		ServerLoadBalancer serverLoadBalancer = new ServerLoadBalancer();
+		Server[] singleServerArray = {new Server(10)};
+		Vm[] vms = new Vm[0];
+
+		serverLoadBalancer.balance(singleServerArray, vms);
+
+		assertEquals(0, singleServerArray[0].getFillPercentage(), 0.001);
+	}
 
 }
