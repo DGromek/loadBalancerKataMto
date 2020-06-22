d47bc493cdc5e7807263543a8e8a11e9b647f3ee
DGromek <dawidgromek.1997@gmail.com>
2020-06-22 23:09:21 +0200

    Second test GREEN

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,9 +1,13 @@
 package edu.iis.mto.serverloadbalancer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
 
     private int capacity;
     private int filledCapacity = 0;
+    private List<Vm> vms = new ArrayList<>();
 
     public Server(int capacity) {
         this.capacity = capacity;
@@ -11,5 +15,14 @@ public class Server {
 
     public double getFillPercentage() {
         return 1.0d * filledCapacity / capacity  * 100;
+    }
+
+    public boolean contains(Vm vm) {
+        return vms.contains(vm);
+    }
+
+    public void addVm(Vm vm) {
+        vms.add(vm);
+        filledCapacity += 10;
     }
 }
